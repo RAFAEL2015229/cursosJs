d9bdcb89023af25a162361b20c625c7c8fad37c5
Rafael <rafaelabrantes966@gmail.com>
2025-09-27 23:35:07 -0300

    Aprendendo a usar condições em javascript e fazendo exercicio para melhorar

---
+++ Aula 11/ex.009.js
@@ -0,0 +1,7 @@
+var pais = 'brasil'
+console.log(`Eu nasci no ${pais}, minha terra natal! `);
+if (pais == 'brasil') {
+    console.log(`Voce e Brasileiro! ${pais}`)
+} else {
+    console.log(`Voce e estrangeiro! `)
+}
---
+++ Aula 11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 55
+console.log(`A velocidade do seu carro é ${vel}Km`);
+
+if (vel >= 60) { // Condição Simples! 
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`);
+} else {
+    console.log(`Voce esta na velocidade permitida. `);
+}
