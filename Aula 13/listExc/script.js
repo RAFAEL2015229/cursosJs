06eeb3a5adc562f328963f4a1c134e6b8aa95246
Rafael <rafaelabrantes966@gmail.com>
2025-10-05 19:20:32 -0300

    comentando o exercicio que eu fiz

--- Aula 13/listExc/script.js
+++ Aula 13/listExc/script.js
@@ -1,21 +1,51 @@
+//Criando uma função para uma lista de tarefas, e estou declarando no momento com um evento de 'click' 
 function lista() {
     var text1 = window.document.getElementById('text1')
     var text2 = window.document.getElementById('text2')
     var text3 = window.document.getElementById('text3')
 
-    var botadd1 = window.document.getElementById('botadd1')
-    var botadd2 = window.document.getElementById('botadd2')
-    var botadd3 = window.document.getElementById('botadd3')
+    var botadd1 = document.querySelector('input#botadd1')
+    var botadd2 = document.querySelector('input#botadd2')
+    var botadd3 = document.querySelector('input#botadd3')
 
-    var add1 = window.document.getElementById('add1')
-    var add2 = window.document.getElementById('add2')+
-    
-    botadd1.addEventListener('click', clicar)
-    function clicar() {
-        var texto = text1.value
-        var criarL = document.createElement('li')
-        criarL.innerHTML = `<strong>${texto}</strong>`
-        document.getElementById(add1.appendChild(criarL))
+    var add1 = document.getElementById('add1')
+    var add2 = document.getElementById('add2')
+    var add3 = document.getElementById('add3')
 
+    botadd1.addEventListener('click', clicar1)
+    botadd2.addEventListener('click', clicar2)
+    botadd3.addEventListener('click', clicar3)
+
+//Com a função criada de evento para clicar criei 3 evento e em cada estrutura e usado um if para adicionar uma <li> e caso venha vazio '' aparecera uma alerta de [ERRO].
+    function clicar1() {
+        var texto1 = text1.value
+
+        if (texto1 == '' && texto2 == '' && texto3 == '') {
+            window.alert('[ERROR] Voce precisa escrever as tarefas!')
+            return;
+        } if (texto1 != '') {
+            var criarL1 = document.createElement('li')
+            criarL1.innerHTML = `<strong>${texto1}</strong>`
+            add1.appendChild(criarL1)
+            add1.style.padding = '10px';
+        }
+    }
+    function clicar2() {
+        var texto2 = text2.value
+        if (texto2 != '') {
+            var criarL2 = document.createElement('li')
+            criarL2.innerHTML = `<strong>${texto2}</strong>`
+            add2.appendChild(criarL2)
+            add2.style.padding = '10px';
+        }
+    }
+    function clicar3() {
+        var texto3 = text3.value
+        if (texto3 != '') {
+            var criarL3 = document.createElement('li')
+            criarL3.innerHTML = `<strong>${texto3}</strong>`
+            add3.appendChild(criarL3)
+            add3.style.padding = '10px';
+        }
     }
 }
--- Aula 14/ex015/script.js
+++ Aula 14/ex015/script.js
@@ -1,3 +1,4 @@
+//Funcao que foi sicronizada do html para verificar a idade da pessoa e se e Homem ou Mulher
 function verificar() {
     var data = new Date();
     var ano = data.getFullYear()
@@ -5,6 +6,7 @@ function verificar() {
     var fano = document.getElementById('texta1')
     var res = document.querySelector('div#res')
 
+    //Informação de [ERRO] caso no texto nao ter informação ou estiver vazio ou alem do ano atual ou se passar dos 105 anos (1920) Para printar uma tela de [ERRO]
     if (fano.value.length == 0 || Number(fano.value) > ano || Number(fano.value) < 1920) {
         window.alert('[ERRO] Verifique os dados e tente novamente.')
     } else {
@@ -14,37 +16,48 @@ function verificar() {
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
 
+        //Sistema para identificar a idade da pessoa e informar se e Criança, Jovem adolecente, adulto ou idoso e com base na idade da pessoa sera printado na tela uma imagem conforme a idade! 
         if (fsex[0].checked) {
             genero = 'Homem'
-            if (idade >= 0 && idade < 10) {
+            if (idade >= 0 && idade < 13) {
                 // Criança 
+                genero = 'criança'
                 img.setAttribute('src', 'foto_criancaMasculino.jpg')
             } else if (idade < 21) {
                 //Jovem adolecente
+                genero = 'adolecente'
                 img.setAttribute('src', 'adolecenteMasc.jpg')
             } else if ( idade < 50) {
                 //Adulto
+                genero = 'adulto'
                 img.setAttribute('src', 'Jovem_adulto.jpg')
             } else {
                 //Idoso
+                genero = 'idoso'
                 img.setAttribute('src' ,'idoso.jpg') 
             }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
             if (idade >= 0 && idade < 10) {
                 // Criança
+                genero = 'crianca'
                 img.setAttribute('src' ,'foto_criancaFeminino.jpg')
             } else if (idade < 21) {
                 //Jovem adolecente
+                genero = 'adolecente'
                 img.setAttribute('src', 'adolecenteFeminina.jpg')
             } else if ( idade < 50) {
                 //adulto
+                genero = 'adulta'
                 img.setAttribute('src', 'jovem_adulta.jpg')
             } else {
                 //idoso
+                genero = 'idosa'
                 img.setAttribute('src' , 'idosa.jpg')
             }
         }
+
+        //Printar na tela Centralizando o Resultado + a imagem que foi importada 
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos que voce e um ${genero} com ${idade} anos`
         res.appendChild(img)
